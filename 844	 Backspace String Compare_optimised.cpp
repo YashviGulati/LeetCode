3a114fec7ba95c58034af684e6c040e8036359e3
Yashvi Gulati <32419771+YashviGulati@users.noreply.github.com>
2020-01-25 18:57:40 +0530

    844

---
+++ 844	 Backspace String Compare_optimised.cpp
@@ -0,0 +1,73 @@
+/*
+Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+
+Example 1:
+
+Input: S = "ab#c", T = "ad#c"
+Output: true
+Explanation: Both S and T become "ac".
+
+Example 2:
+
+Input: S = "ab##", T = "c#d#"
+Output: true
+Explanation: Both S and T become "".
+
+Example 3:
+
+Input: S = "a##c", T = "#a#c"
+Output: true
+Explanation: Both S and T become "c".
+
+Example 4:
+
+Input: S = "a#c", T = "b"
+Output: false
+Explanation: S becomes "c" while T becomes "b".
+
+Note:
+
+    1 <= S.length <= 200
+    1 <= T.length <= 200
+    S and T only contain lowercase letters and '#' characters.
+
+Follow up:
+
+    Can you solve it in O(N) time and O(1) space?
+
+*/
+
+class Solution {
+public:
+    bool backspaceCompare(string S, string T) {
+        int i=S.length()-1;
+        int j=T.length()-1;
+        int flag=0;
+        while(i>=0){
+            if(S[i]=='#'){
+                flag++;
+                S.erase(S.begin()+i);
+            }
+            else if(flag>0){
+                S.erase(S.begin()+i);
+                flag--;
+            }
+            i--;
+        }
+        flag=0;
+        while(j>=0){
+            if(T[j]=='#'){
+                flag++;
+                T.erase(T.begin()+j);
+            }
+            else if(flag>0){
+                T.erase(T.begin()+j);
+                flag--;
+            }
+            j--;
+        }
+        if(S==T)
+            return true;
+        return false;
+        }
+};
